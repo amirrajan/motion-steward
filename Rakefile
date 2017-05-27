38bf3559dd18862d2a77bc2fcc3f1a019c89e5ae
Amir Rajan <ar@amirraja.net>
2017-05-27 10:07:20 -0500

    working through audit of development profiles and devices

--- Rakefile
+++ Rakefile
@@ -1,5 +1,14 @@
-task :default do
-  system 'gem uninstall motion-steward'
+def reinstall
+  system 'gem uninstall -ax motion-steward'
   system 'gem build motion-steward.gemspec'
   system 'gem install ./motion-steward-1.0.3.gem'
+end
+
+task :build do
+  reinstall
+end
+
+task :default do
+  reinstall
+  system 'motion-steward'
 end
--- lib/motion-steward.rb
+++ lib/motion-steward.rb
@@ -1,16 +1,63 @@
+# coding: utf-8
+require 'fastlane'
+
 class MotionSteward
+  def self.udid_of_connected_devices
+    script = <<~SCRIPT
+    i=0
+    for line in $(system_profiler SPUSBDataType | sed -n -e '/iPad/,/Serial/p' -e '/iPhone/,/Serial/p' | grep "Serial Number:" | awk -F ": " '{print $2}'); do
+        UDID=${line}
+        echo $UDID
+        udid_array[i]=${line}
+        i=$(($i+1))
+    done
+
+    cnt=${#udid_array[@]}
+    for ((i=0;i<cnt;i++)); do
+        echo ${udid_array[i]}
+    done
+ SCRIPT
+
+    result = `#{script}`.chomp
+    return nil if result.chomp.length.zero?
+    result
+  end
+
   def self.audit_provisioning_profiles
     apps = Spaceship.app.all
     profiles = Spaceship.provisioning_profile.all
     development_profiles = profiles.find_all { |p| p.is_a? Spaceship::Portal::ProvisioningProfile::Development }
     distribution_profiles = profiles.find_all { |p| p.is_a? Spaceship::Portal::ProvisioningProfile::AppStore }
 
-    apps.find_all do |a|
+    lookup = apps.map do |a|
       {
         app: a,
         development_profile: development_profiles.find_all { |d| d.app.app_id == a.app_id },
         distribution_profile: distribution_profiles.find_all { |d| d.app.app_id == a.app_id }
       }
+    end
+
+    currently_connected_device = udid_of_connected_devices
+
+    lookup.each do |a|
+      puts "#{a[:app].name} audit:"
+      if a[:development_profile].any?
+        if a[:development_profile].first.devices.none? { |d| d.udid == currently_connected_device }
+          if currently_connected_device
+            puts '  ✗ Development profile is missing the device that is currently connected.'
+          end
+        else
+          puts '  ✔ All is good with the development profile.'
+        end
+      else
+        puts '  ✗ Development profile missing.'
+      end
+
+      if a[:distribution_profile].any?
+        puts '  ✔ All is good with the distribution profile.'
+      else
+        puts '  ✗ Distribution profile missing.'
+      end
     end
   end
 end
